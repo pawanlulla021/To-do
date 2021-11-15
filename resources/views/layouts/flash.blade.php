fc90ef8a8a33f84951819bd7e707c4efe4a11b8f
Pawan Lulla <pawanlulla021@gmail.com>
2021-11-15 10:39:18 +0530

    adding changes

--- resources/views/layouts/flash.blade.php
+++
@@ -1,8 +0,0 @@
-@if(session()->has('message'))
-                       <div class="alert alert-success">{{session()->get('message')}}</div>
-                       {{session()->forget('message')}}
-                   @elseif(session()->has('error'))
-                       <div class="alert alert-danger">{{session()->get('error')}}</div>
-                       {{session()->forget('error')}}
-
-                   @endif    
--- resources/views/livewire/counter.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-
-<button wire:click="increment">+</button>
-    <h1>{{ $count }}</h1>
-<button wire:click="decrement">-</button>    
-</div>
